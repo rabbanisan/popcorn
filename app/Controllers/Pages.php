16dd71b1d5b75b9568fb32d54c4392d7ae75a301
rabbanisan <bani.ramadhan99@gmail.com>
2021-11-19 08:08:50 +0700

    first commit

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -2,10 +2,33 @@
 
 namespace App\Controllers;
 
+use App\Models\....;
+
 class Pages extends BaseController
 {
-    public function index()
+    protected $movieModel;
+    public function __....()
     {
-        echo view('pages/home');
+        $this->movieModel = new ....();
+    }
+
+    public function ....()
+    {
+        $nowPlaying = $this->movieModel->where(.... , ....)->findAll();
+        $upComing = $this->movieModel->where(... , ....)->findAll();;
+        $data = [
+            .... => $nowPlaying,
+            .... => $upComing
+        ];
+        echo view('pages/home', ....);
+    }
+
+    public function ....($....)
+    {
+        $movie = $this->movieModel->where(.... , $....)->findAll();
+        $data = [
+            .... => $movie
+        ];
+        echo view('pages/movie', $data);
     }
 }
