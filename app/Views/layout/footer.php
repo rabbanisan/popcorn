b9c32e213dfadc4c2db47b895ed26037bd41240b
supersyzen <84361552+supersyzenn@users.noreply.github.com>
2021-11-20 15:53:51 +0700

    Update footer.php
    Ahmad Juaeni Yunus

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -2,7 +2,7 @@
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div class="col-md-4 d-flex align-items-center">
             <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                <img class="footer-brand" src=.... alt="">
+                <img class="footer-brand" src="/assets/icon/popcorn1.png" alt="">
             </a>
             <span class="text-muted">© 2021 Popcorn, Inc</span>
         </div>
@@ -10,15 +10,15 @@
         <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li class="ms-3">
                 <a class="text-muted" href="#"></a>
-                <img src=.... alt="">
+                <img src="/assets/icon/twitter.png" alt="">
             </li>
             <li class="ms-3">
                 <a class="text-muted" href="#"></a>
-                <img src=.... alt="">
+                <img src="/assets/icon/instagram.png" alt="">
             </li>
             <li class="ms-3">
                 <a class="text-muted" href="#"></a>
-                <img src=.... alt="">
+                <img src="/assets/icon/facebook.png" alt="">
             </li>
         </ul>
     </footer>
