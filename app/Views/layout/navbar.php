b007922794a9150bfbf8a6090fd4877549a9fb74
supersyzen <84361552+supersyzenn@users.noreply.github.com>
2021-11-20 16:04:59 +0700

    Update navbar.php
    Ahmad Juaeni Yunus

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-lg-top">
     <div class="container">
         <a class="navbar-brand align-item-center" href="/">
-            <img src=.... alt="tv" width=" 40" height="40">
+            <img src="/assets/icon/popcorn1.png" alt="tv" width=" 40" height="40">
             Popcorn
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,19 +13,19 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="/#now-playing-section">
-                        <img src=.... alt="" width="20" height="20">Now Playing
+                        <img src=/assets/icon/play.svg" alt="" width="20" height="20">Now Playing
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/#upcoming-section">
-                        <img src=.... alt="" width="20" height="20">Upcoming
+                        <img src="/assets/icon/corner-right-up.svg" alt="" width="20" height="20">Upcoming
                     </a>
                 </li>
             </ul>
             <div class="img-powered mx-lg-5">
-                <img src=.... alt="" width="30" height="30" class="mx-3">
-                <img src=.... alt="" width="30" height="30" class="mx-3">
-                <img src=.... alt="" width="75" height="25" class="mx-3">
+                <img src="/assets/icon/cinema.png" alt="" width="30" height="30" class="mx-3">
+                <img src="/assets/icon/imax.png" alt="" width="30" height="30" class="mx-3">
+                <img src="/assets/icon/logo_gabungan_3_05.png" alt="" width="75" height="25" class="mx-3">
             </div>
         </div>
     </div>
